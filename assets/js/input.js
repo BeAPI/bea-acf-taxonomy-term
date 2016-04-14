31f3334b14eb85f6827698fa3d95a3aef0df762a
tweetpressfr <jmaury@beapi.fr>
2016-04-14 23:28:53 +0200

    js refactoring

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,44 +1,14 @@
-var bea_acf_taxonomy_terms_tpl, bea_acf_taxonomy_terms_ajax;
-jQuery(function() {
-	bea_acf_taxonomy_terms_tpl = jQuery( '#tmpl-bea-taxonomy-term').html();
-});
-function bea_acf_term_taxonomy_refresh() {"use strict";
-
-    jQuery('select.bea_acf_taxonomy_term_taxonomies, select.bea_acf_taxonomy_term_taxonomies_terms')
-        .select2()
-        .on('change', function (e) {
-            "use strict";
-            var select = jQuery(e.target);
-            if (select.hasClass('bea_acf_taxonomy_term_taxonomies')) {
-                if (!_.isUndefined(bea_acf_taxonomy_terms_ajax)) {
-                    bea_acf_taxonomy_terms_ajax.abort();
-                }
-                bea_acf_taxonomy_terms_ajax = jQuery.ajax({
-                    url: ajaxurl,
-                    type: "POST",
-                    dataType: 'json',
-                    data: {
-                        action: 'bea_acf_taxonomy_term',
-                        _ajax_nonce: bea_acf_taxonomy_term.nonce,
-                        taxonomies: e.val
-                    }
-                }).success(function (response) {
-                    var terms_select = select.parent().find('select.bea_acf_taxonomy_term_taxonomies_terms'),
-                        content = _.template(bea_acf_taxonomy_terms_tpl, {
-                            terms: response.data,
-                            selected_terms: terms_select.val()
-                        });
-                    terms_select.html(content).select2("val", terms_select.val());
-                });
-            }
-        });
-}
-
 /**
  * @link https://github.com/elliotcondon/acf-field-type-template/blob/master/assets/js/input.js
+ * TODO: DRY
  */
 
 (function($){
+
+    var bea_acf_taxonomy_terms_tpl, bea_acf_taxonomy_terms_ajax;
+    jQuery(function() {
+        bea_acf_taxonomy_terms_tpl = jQuery( '#tmpl-bea-taxonomy-term').html();
+    });
 
 
     function initialize_field( $el ) {
@@ -70,7 +40,34 @@ function bea_acf_term_taxonomy_refresh() {"use strict";
             acf.get_fields({ type : 'taxonomy_term'}, $el).each(function(){
 
                 initialize_field( $(this) );
-                bea_acf_term_taxonomy_refresh();
+                $el.find('select.bea_acf_taxonomy_term_taxonomies, select.bea_acf_taxonomy_term_taxonomies_terms')
+                    .select2()
+                    .on('change', function (e) {
+                        "use strict";
+                        var select = jQuery(e.target);
+                        if (select.hasClass('bea_acf_taxonomy_term_taxonomies')) {
+                            if (!_.isUndefined(bea_acf_taxonomy_terms_ajax)) {
+                                bea_acf_taxonomy_terms_ajax.abort();
+                            }
+                            bea_acf_taxonomy_terms_ajax = jQuery.ajax({
+                                url: ajaxurl,
+                                type: "POST",
+                                dataType: 'json',
+                                data: {
+                                    action: 'bea_acf_taxonomy_term',
+                                    _ajax_nonce: bea_acf_taxonomy_term.nonce,
+                                    taxonomies: e.val
+                                }
+                            }).success(function (response) {
+                                var terms_select = select.parent().find('select.bea_acf_taxonomy_term_taxonomies_terms'),
+                                    content = _.template(bea_acf_taxonomy_terms_tpl, {
+                                        terms: response.data,
+                                        selected_terms: terms_select.val()
+                                    });
+                                terms_select.html(content).select2("val", terms_select.val());
+                            });
+                        }
+                    });
 
             });
 
@@ -100,7 +97,34 @@ function bea_acf_term_taxonomy_refresh() {"use strict";
             $(postbox).find('.field[data-field_type="taxonomy_term"]').each(function(){
 
                 initialize_field( $(this) );
-                bea_acf_term_taxonomy_refresh();
+                jQuery('select.bea_acf_taxonomy_term_taxonomies, select.bea_acf_taxonomy_term_taxonomies_terms')
+                    .select2()
+                    .on('change', function (e) {
+                        "use strict";
+                        var select = jQuery(e.target);
+                        if (select.hasClass('bea_acf_taxonomy_term_taxonomies')) {
+                            if (!_.isUndefined(bea_acf_taxonomy_terms_ajax)) {
+                                bea_acf_taxonomy_terms_ajax.abort();
+                            }
+                            bea_acf_taxonomy_terms_ajax = jQuery.ajax({
+                                url: ajaxurl,
+                                type: "POST",
+                                dataType: 'json',
+                                data: {
+                                    action: 'bea_acf_taxonomy_term',
+                                    _ajax_nonce: bea_acf_taxonomy_term.nonce,
+                                    taxonomies: e.val
+                                }
+                            }).success(function (response) {
+                                var terms_select = select.parent().find('select.bea_acf_taxonomy_term_taxonomies_terms'),
+                                    content = _.template(bea_acf_taxonomy_terms_tpl, {
+                                        terms: response.data,
+                                        selected_terms: terms_select.val()
+                                    });
+                                terms_select.html(content).select2("val", terms_select.val());
+                            });
+                        }
+                    });
 
             });
 
