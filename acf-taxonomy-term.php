1b07bf5b068555a843bb63adcc42b3432add4a38
Julien Maury <6878027+jmau111@users.noreply.github.com>
2018-08-02 10:44:45 +0200

    increment plugin version according to tag

--- acf-taxonomy-term.php
+++ acf-taxonomy-term.php
@@ -1,12 +1,12 @@
 <?php
 /**
-Plugin Name: Advanced Custom Fields: Taxonomy term
-Description: Add taxonomies terms selector
-Version: 1.0.1
-Author: BeApi
-Author URI: www.beapi.fr
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+* Plugin Name: Advanced Custom Fields: Taxonomy term
+* Description: Add taxonomies terms selector
+* Version: 1.0.3
+* Author: BeApi
+* Author URI: www.beapi.fr
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
